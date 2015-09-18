3c9d9c416344c8fdffecfd273f1ff368f1ddc1e3
atlisg12 <atlisg12@ru.is>
2015-09-18 15:04:08 +0000

    með föllum en virkar ekki LOL

--- src/tftpd.c
+++ src/tftpd.c
@@ -16,6 +16,82 @@
 #include <stdio.h>
 #include <fcntl.h>
 
+/* TFTP Formats */
+
+// Read request (RRQ) / Write request (WRQ)
+typedef struct RRQ_WRQ {
+       	u_short opcode; // Op #01/02 (2 bytes)
+       	char filename[32]; // Filename (string)
+       	u_char pad1; // Padding (1 byte)
+       	char mode[32]; // Mode (string)
+       	u_char pad2; // Padding (1 byte)
+} RRQ_WRQ;
+
+// Data (DATA)
+typedef struct DATA {
+       	u_short opcode; // Op #03 (2 bytes)
+       	u_short blocknr; // Block # (2 bytes)
+       	char data[512]; // Data (n bytes)
+	u_short datasize; // number of bytes
+} DATA;
+	
+// Acknowledgment (ACK)
+typedef struct ACK {
+        u_short opcode; // Op #04 (2 bytes)
+        u_short blocknr; // Block # (2 bytes)
+} ACK;
+
+// Error (ERROR)
+typedef struct ERROR {
+        u_short opcode; // Op #05 (2 bytes)
+        u_short errorcode; // ErrorCode (2 bytes)
+        char errmsg[32]; // ErrMsg (string)
+        u_char pad; // Padding (1 byte)
+} ERROR;
+
+char *assemble_msg(DATA m)
+{
+	char msg[516];
+	u_short i;
+	msg[0] = m.opcode >> 8;
+	msg[1] = m.opcode; 
+	msg[2] = m.blocknr >> 8;
+	msg[3] = m.blocknr;
+	for (i = 0; i < m.datasize; i++) {
+		msg[i+4] = m.data[i];
+	}
+	return *msg;
+}
+
+ssize_t read_file(FILE* fp, RRQ_WRQ rrq, char *buffer) 
+{
+	// Read from file and put the content in buffer
+	ssize_t size;
+	size = fread(buffer, 1, 303508, fp);
+	fprintf(stdout, "size: %d\n", size);
+	return size;
+}
+
+DATA *chop_it(char *buffer, ssize_t size)
+{
+	// Chop the buffer up in pieces:
+	u_short i, j;
+	DATA data;
+	DATA data_blocks[586];
+	for (i = 0; i * 512 < size; i++) {
+		data.opcode = 0x3;
+		data.blocknr = i+1;
+		for (j = 0; j < 512 && i * 512 + j < size; j++) {
+			data.data[j] = buffer[i * 512 + j];
+		}
+		data.datasize = j;
+		data_blocks[i] = data;
+	}
+	fprintf(stdout, "i: %d\n", i);
+	return data_blocks;
+}
+DATA *list_of_data_blocks;
+
 int main(int argc, char **argv)
 {
 	int sockfd;
@@ -23,7 +99,7 @@ int main(int argc, char **argv)
         char message[512];
 	short port;
 	char dir[30];
-	char buffer[300000];
+	char buffer[303508];
 	
 	/* Put arguments in variables and change working directory */
 	sscanf(argv[1], "%d", &port);
@@ -39,41 +115,7 @@ int main(int argc, char **argv)
         server.sin_addr.s_addr = htonl(INADDR_ANY);
 	server.sin_port = htons(port);
         bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));
-
-	/* TFTP Formats */
-
-	// Read request (RRQ) / Write request (WRQ)
-	typedef struct RRQ_WRQ {
-        	u_short opcode; // Op #01/02 (2 bytes)
-        	char filename[32]; // Filename (string)
-        	u_char pad1; // Padding (1 byte)
-        	char mode[32]; // Mode (string)
-        	u_char pad2; // Padding (1 byte)
-        } RRQ_WRQ;
-
-	// Data (DATA)
-	typedef struct DATA {
-        	u_short opcode; // Op #03 (2 bytes)
-        	u_short blocknr; // Block # (2 bytes)
-        	char data[512]; // Data (n bytes)
-        } DATA;
-	
-        // Acknowledgment (ACK)
-        typedef struct ACK {
-        	u_short opcode; // Op #04 (2 bytes)
-        	u_short blocknr; // Block # (2 bytes)
-        } ACK;
-
-        // Error (ERROR)
-        typedef struct ERROR {
-        	u_short opcode; // Op #05 (2 bytes)
-        	u_short errorcode; // ErrorCode (2 bytes)
-        	char errmsg[32]; // ErrMsg (string)
-        	u_char pad; // Padding (1 byte)
-        } ERROR;
-	
-	DATA list_of_data_blocks[10000];
-	
+		
 	for (;;) {
                 fd_set rfds;
                 struct timeval tv;
@@ -122,40 +164,16 @@ int main(int argc, char **argv)
 				// Output of the server:
 				fprintf(stdout, "file \"%s\" requested from 127.0.0.1:%d\n", 
 									rrq.filename, port);
-
-				// Open the requested file and put the content in buffer
 				FILE* fp;
 				fp = fopen(rrq.filename, "rb");
-				ssize_t size;
-				size = fread(buffer, 1, 300000, fp);
-				fclose(fp);
-				fprintf(stdout, "size: %d\n", size);
+				ssize_t bytes_read = read_file(fp, rrq, buffer);
+				list_of_data_blocks = chop_it(buffer, bytes_read);
 				
-				// Chop the buffer up in pieces:
-				for (i = 0; i * 512 < size; i++) {
-					DATA data;
-					data.opcode = 0x3;
-					data.blocknr = i+1; // hér
-					for (j = 0; j < 512 && i * 512 + j < size; j++) {
-						data.data[j] = buffer[i * 512 + j];
-					}
-					if (j < 512) {
-						data.data[j] = '\0';
-					}
-					list_of_data_blocks[i] = data;
-				}
-
 				// Send first DATA pack:
-				char msg[516];
 				DATA m = list_of_data_blocks[0];
-				msg[0] = m.opcode >> 8;
-				msg[1] = m.opcode; 
-				msg[2] = m.blocknr >> 8;
-				msg[3] = m.blocknr;
-				for (i = 0; i < 512; i++) {
-					msg[i+4] = m.data[i];
-				}
-				sendto(sockfd, msg, (size_t) sizeof(msg), 0, 
+				char *msg = assemble_msg(m);
+				sendto(sockfd, msg,
+					(size_t) m.datasize+4, 0, 
 					(struct sockaddr *) &client,
 					(socklen_t) sizeof(client));
 			}
@@ -163,25 +181,20 @@ int main(int argc, char **argv)
 			if (message[1] == 0x4) {
 				// ACK
 				// Fill sruct
+				fprintf(stdout, "ACKNIE");
 				ACK ack;
 				ack.opcode = message[1];
 				u_char a, b;
 				a = message[2];
 				b = message[3];
 				ack.blocknr = (a << 8) | b;
+				// if buffer finished then chop
 				DATA m = list_of_data_blocks[ack.blocknr];
+				char *msg = assemble_msg(m);
 
 				// Send next block
-				char msg[516];
-				msg[0] = m.opcode >> 8;
-				msg[1] = m.opcode; 
-				msg[2] = m.blocknr >> 8;
-				msg[3] = m.blocknr;
-				u_short i;
-				for (i = 0; i < 512 && m.data[i] != '\0'; i++) {
-					msg[i+4] = m.data[i];
-				}
-				sendto(sockfd, msg, (size_t) i+4, 0,
+				sendto(sockfd, &msg,
+					(size_t) m.datasize+4, 0,
 					(struct sockaddr *) &client,
 					(socklen_t) sizeof(client));
 			}
